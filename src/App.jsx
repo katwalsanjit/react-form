894328585cdd6f9654d93487e7c7cc21fee59f05
katwalsanjit <sanjitkatwal@gmail.com>
2024-09-03 21:38:02 -0400

    install react with tailwind css and react router

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Dashboard from './components/Dashboard'
+import MainForm from './components/MainForm'
+
+function App() {
+  return (
+    <>
+      <Dashboard />
+    </>
+  )
+}
+
+export default App
